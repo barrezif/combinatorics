3ab1c5275985aa8c721bf0c8431f87785bb4c240
barrezif <barrezif@gmail.com>
2024-06-23 18:11:25 -0400

    Finished get_partitions function

--- ThreeEventStats.py
+++ ThreeEventStats.py
@@ -1,14 +1,13 @@
+from VennGraph import VennGraph
+from VennElem import VennElem
+from VennSet import VennSet
 from collections import defaultdict, deque
 
 type VennSetName = str | tuple[str, ...]
 type GraphSet = dict[str, 'VennSet']
 
-from VennSet import VennSet
-from VennElem import VennElem
-from VennGraph import VennGraph
 
 ALLOWED_VENN_GRAPH_SIZES = [2, 3, 8]
-
 
 
 class Domino:
@@ -51,16 +50,7 @@ three_set_venn_diagram.put_in_set([all_dominoes[1]], p1)
 three_set_venn_diagram.put_in_set([all_dominoes[5]], p1, p2)
 three_set_venn_diagram.remove_from_graph([all_dominoes[0]])
 
-
-class SubPartition:
-    """ IDK - a partition is a way to build the whole set by chunks? In this case
-    we care about the composition of the whole set by which components
-    of which subsets were used. I want this class to refer to one group's make
-    up. The chunk in reference to subsets that make up one group's part
-    of the partition. So I think subpartition might be a better name. """
-    pass
-
-def get_subpartitions(group_name: VennSetName, state: VennGraph, handsize: int) -> [SubPartition]:
+def get_subpartitions(group_name, state: VennGraph, handsize: int):
     """ Gets all valid subpartitions for a group given a graph and size.
     Will prioritize picking from the subset only it can take from first and
     build the rest of the combinations off of that. If it can't include
@@ -68,40 +58,60 @@ def get_subpartitions(group_name: VennSetName, state: VennGraph, handsize: int) 
     if there aren't enough elements outside of that group to fill
     its hand, then we return an empty list."""
 
-    remainder = handsize - state.size(True, group_name)
-    subset_sizes = sorted(list(state.get_subset_sizes(group_name).items()))
-    if  remainder < 0: return []
-    if remainder == 0: return [(group_name, handsize)]
+    this_set_size = 0
+    this_subsets = []
+    not_this_subsets = []
+
+    for sub_graph in state.keys():
+        if tuple(group_name) == sub_graph:
+            this_set_size = state[sub_graph]
+        elif group_name in sub_graph:
+            this_subsets.append([sub_graph, state[sub_graph]])
+        else:
+            not_this_subsets.append([sub_graph, state[sub_graph]])
+
+    remainder = handsize - this_set_size
+    subset_sizes = sorted(this_subsets)
+    if remainder < 0:
+        return []
+    if remainder == 0:
+        return [(group_name, handsize)]
     all_subpartitions = []
-    for change in generate_changes(subset_sizes, remainder, 0, {ss[0]:0 for ss in subset_sizes}):
-        change[group_name] = handsize-remainder
+    for change in generate_changes(subset_sizes, remainder, 0, {ss[0]: 0 for ss in subset_sizes}):
+        change[(group_name,)] = handsize-remainder
         all_subpartitions.append(change)
 
     return all_subpartitions
 
 
 def get_partitions(state: VennGraph, handsizes: deque):
-    size_state = {s:state._get_set_from_names(s).size() for s in state.get_all_set_names()}
+    size_state = {s: state._get_set_from_names(
+        s).size() for s in state.get_all_set_names()}
     states = deque([size_state])
+    partitions = deque([[]])
     while states:
-        print(states)
         if not handsizes:
-            return
+            return partitions
         player, hand_size = handsizes.popleft()
         player_states_size = len(states)
         for _ in range(player_states_size):
             curr_state = states.popleft()
+            curr_partition = partitions.popleft()
             for player_partition in get_subpartitions(player, curr_state, hand_size):
-                states.append(apply_state_changes(player_partition, curr_state))
+                partition_copy = curr_partition.copy()
+                partition_copy.append(player_partition)
+                partitions.append(partition_copy)
+                states.append(apply_state_changes(
+                    player_partition, curr_state.copy()))
 
 
 
-def apply_state_changes(option, state: VennGraph):
-    """ Return a new VennGraph with the changes, or just modify
-    the current graph. Let's try that. I just wouldn't want to edit it
-    and have a bug that makes it so that we can't get it back to
-    the original state."""
-    pass
+def apply_state_changes(changes, state: VennGraph):
+    """ Return a new VennGraph with the changes"""
+    for k, v in changes.items():
+        state[k] = state[k] - v
+    return state
+
 
 def generate_changes(sizes, hand_size, idx, res):
     if idx >= len(sizes) and hand_size != 0:
@@ -117,10 +127,38 @@ def generate_changes(sizes, hand_size, idx, res):
         if changes:
             yield from changes
 
+for partition in get_partitions(three_set_venn_diagram, deque([[p1, 6], [p2, 6], [p3,6]])):
+    print("========================")
+    print(partition)
 
-print("------------")
 
-# for subpartition in get_subpartitions(p1, three_set_venn_diagram, 4):
-#     print(subpartition)
 
-get_partitions(three_set_venn_diagram, deque([[p1, 6]]))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+The Data is the collection of subsets that make up the venn diagram, the mapping of
+dominoes to venn diagram subset, and the number of elements each parent set must have
+to make their hand.
+
+
+Parent Sets -> Subsets
+Subsets -> Dominos
+A combination is 
+
+
+
+
+"""
